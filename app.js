778c434156ff79bc1097aa45cb8c8446f0ad49e1
prkm93 <aradshub93@gmail.com>
2021-09-02 08:51:02 +0530

    added initial files

---
+++ app.js
@@ -0,0 +1,9 @@
+const btnTranslate = document.querySelector('#btn-translate');
+const textInput = document.querySelector('#text-input');
+
+console.log(btnTranslate);
+console.log(textInput);
+
+btnTranslate.addEventListener('click', function(){
+    console.log('clicked');
+})
